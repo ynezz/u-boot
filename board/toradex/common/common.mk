f883e98833b5c37cae97c7678f170f84c20b6eb7
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2015-04-16 18:28:15 +0200

    colibri_vf: change toradex config block offset type from hex to int
    Allow specifying a negative number as the Toradex config block offset
    by changing its type from hex to int. This is required in the eMMC case
    where this is used to force a location at the end of respective
    hardware boot partition.
    
    While at it fix SPL compilation by not compiling configblock handling
    in this case for now.

--- board/toradex/common/common.mk
+++ board/toradex/common/common.mk
@@ -1,2 +1,7 @@
 # Common for all Toradex modules
+ifeq ($(CONFIG_SPL_BUILD),y)
+# necessary to create built-in.o
+obj- := __dummy__.o
+else
 obj-$(CONFIG_TRDX_CFG_BLOCK) += ../common/configblock.o
+endif
