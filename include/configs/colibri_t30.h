0d4498ae8b535155f79473d8eed522f18420744f
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2015-10-26 14:02:52 +0100

    apalis/colibri_t30: move env end of emmc boot area before config block
    Move the U-Boot environment before the config block at the end of the
    first eMMC (hardware) boot area partition to be again more in line with
    how this got recently changed on Apalis/Colibri iMX6.

--- include/configs/colibri_t30.h
+++ include/configs/colibri_t30.h
@@ -47,11 +47,12 @@
 #define CONFIG_TEGRA_MMC
 #define CONFIG_CMD_MMC
 
-/* Environment in eMMC, at the end of 2nd "boot sector" */
+/* Environment in eMMC, before config block at the end of 1st "boot sector" */
 #define CONFIG_ENV_IS_IN_MMC
-#define CONFIG_ENV_OFFSET		(-CONFIG_ENV_SIZE)
+#define CONFIG_ENV_OFFSET		(-CONFIG_ENV_SIZE + \
+					 CONFIG_TRDX_CFG_BLOCK_OFFSET)
 #define CONFIG_SYS_MMC_ENV_DEV		0
-#define CONFIG_SYS_MMC_ENV_PART		2
+#define CONFIG_SYS_MMC_ENV_PART		1
 
 /* USB client support */
 #define CONFIG_G_DNL_MANUFACTURER	"Toradex"
