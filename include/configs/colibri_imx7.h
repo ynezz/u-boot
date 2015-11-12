f53da57b51757b5ee4ca5faf30994668d6ed281a
Max Krummenacher <max.krummenacher@toradex.com>
2015-11-12 19:55:38 +0100

    colibri_imx7.h: set fdt_file for Colibri iMX7
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- include/configs/colibri_imx7.h
+++ include/configs/colibri_imx7.h
@@ -255,7 +255,7 @@
         "splashpos=m,m\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx7d-sdb.dtb\0" \
+	"fdt_file=imx7d-colibri.dtb\0" \
 	"fdt_addr=0x83000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
