ce0b74beb3c45b16be8672a9ba15c22f7096a4c1
Petr Štetiar <ynezz@true.cz>
2015-12-01 15:01:14 +0100

    Apalis iMX6: fix production commands
    commit 2da77958e96151295e72f8f85aee352d0b4010fe added a configuration
    option to include commands used during production.
    
    Fixing the use of the CONFIG macro.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- board/toradex/apalis_imx6/apalis_imx6.c
+++ board/toradex/apalis_imx6/apalis_imx6.c
@@ -32,7 +32,7 @@
 #include <i2c.h>
 
 #include "../common/configblock.h"
-#ifdef TRDX_CMD_IMX_MFGR
+#ifdef CONFIG_TRDX_CMD_IMX_MFGR
 #include "pf0100.h"
 #endif
 
@@ -785,7 +785,7 @@ int board_init(void)
 	setup_i2c(1, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info_loc);
 	setup_i2c(2, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info3);
 
-#ifdef TRDX_CMD_IMX_MFGR
+#ifdef CONFIG_TRDX_CMD_IMX_MFGR
 	(void) pmic_init();
 #endif
 
